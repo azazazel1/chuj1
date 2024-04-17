497a78f30889b6867d303c191ed979616b9ac1a2
Kuba <zsttpkosmowski@gmail.com>
2024-04-17 13:39:45 +0200

    Nowy raport

--- pierdolnik2.sh
+++ pierdolnik2.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-# Zmienne
-sciezka="/home/kuba/chuj1/"  # Ścieżka do katalogu repozytorium
-
 # Wykonanie skryptu pierdolnik1.sh
 /home/kuba/1/pierdolnik1.sh
 
@@ -13,7 +10,7 @@ aktualna_data=$(date +"%Y-%m-%d_%H-%M-%S")
 nazwa_pliku="PLIK_${USER}_${aktualna_data}.txt"
 
 # Przeniesienie pliku wynikowego do katalogu repozytorium
-mv "/home/$USER/1/PLIK_${USER}_$aktualna_data" "$sciezka$nazwa_pliku"
+mv "/home/$USER/1/PLIK_${USER}_$aktualna_data" "/home/$USER/chuj1/$nazwa_pliku"
 
 # Dodanie i wysłanie zmian do GitHuba
-cd "$sciezka" && git add . && git commit -m "Nowy raport" && git push origin main
+cd "/home/$USER/chuj1/" && git add . && git commit -m "Nowy raport" && git push origin main
